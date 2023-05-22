a54d3b81ce5762234e714d46379a955c021a629b
matinraisi <raisi.m630@gmail.com>
2023-05-22 18:17:16 +0330

    link page login with signup page in front

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'login_page'
+# app_name = 'login_page'
 
 urlpatterns = [
     path('',views.login_page,name='login_page'),
+    path('singup/',views.singup_page,name='singup_page'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 def login_page(request ):
     return render(request ,'login.html')
     
+
+
+def singup_page(request ):
+    return render(request ,'login/signup.html')
     
    
 
